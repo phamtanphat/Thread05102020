83ce8130e4714d863f9e71a5a13763bdd3d057da
phatdroid94 <phatdroid94@gmail.com>
2020-12-07 20:44:54 +0700

    dead block

--- app/src/main/java/com/example/thread05102020/MainActivity.java
+++ app/src/main/java/com/example/thread05102020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thread05102020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                showLog("A");
+            }
+        });
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                showLog("B");
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+    }
+    private void showLog(String name){
+        for (int i = 0; i < 1000 ; i++) {
+            Log.d("BBB", name + " : " + i);
+        }
     }
 }
